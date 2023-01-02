75343252a395d4c2a44e3c25ca142dcd31328f2d
saalu <saaluissaka@gmail.com>
2023-01-02 20:58:28 +0000

    Crud Operations

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
+const blogCtrl = require("../controllers/blogCtrl");
+const auth = require("../middleware/auth");
+
+router.route("/").get(auth, blogCtrl.getBlogs).post(auth, blogCtrl.createBlogs);
 
 router
-  .route("/")
-  .get((req, res) => {
-    res.json({ msg: "Get all blocks heres.." });
-  })
-  .post();
-
-router.route("/:id").get().put().delete();
+  .route("/:id")
+  .get(auth, blogCtrl.getBlog)
+  .put(auth, blogCtrl.updateBlog)
+  .delete(auth, blogCtrl.deleteBlog);
 
 module.exports = router;
